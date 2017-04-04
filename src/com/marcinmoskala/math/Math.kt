8411fc2678db22b86b7cfddf8ae68cb749b914fb
Marcin Moskala <marcinmoskala@gmail.com>
2017-04-04 19:15:08 +0200

    There is 0 permutations of empty set.

--- src/com/marcinmoskala/math/Math.kt
+++ src/com/marcinmoskala/math/Math.kt
@@ -1,3 +1,6 @@
 package com.marcinmoskala.math
 
-val <T> Set<T>.permutationNumber: Int get() = 1
+val <T> Set<T>.permutationNumber: Int get() = when {
+    size == 0 -> 0
+    else -> 1
+}
--- src/com/marcinmoskala/math/PermutationTest.kt
+++ src/com/marcinmoskala/math/PermutationTest.kt
@@ -4,8 +4,12 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class PermutationTest {
-    @Test
-    fun `There is 1 permutation of single element set`() {
+
+    @Test fun `There is 0 permutations of empty set`() {
+        assertEquals(0, setOf<Int>().permutationNumber)
+    }
+
+    @Test fun `There is 1 permutation of single element set`() {
         assertEquals(1, setOf(1).permutationNumber)
     }
 }
