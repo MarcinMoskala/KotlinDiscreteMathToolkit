7eb2d402b5eb889d6d4bbdbde5ffb6f6df377afd
Marcin Moskala <marcinmoskala@gmail.com>
2017-04-05 01:25:46 +0200

    Test products of different Int Collections.

--- src/com/marcinmoskala/math/ProductTest.kt
+++ src/com/marcinmoskala/math/ProductTest.kt
@@ -5,10 +5,6 @@ import org.junit.jupiter.api.Test
 
 internal class ProductTest {
 
-    val setToProduct = mapOf(
-            listOf(1,2,3) to 6
-    )
-
     @Test
     fun `Product of empty IntRange is 0`() {
         Assertions.assertEquals(0, (1..0).product())
@@ -16,8 +12,27 @@ internal class ProductTest {
 
     @Test
     fun `Test products of different IntRanges`() {
-        val rangeToProduct = mapOf(2..4 to 24, 1..4 to 24, 3..4 to 12, 100..100 to 100)
+        val rangeToProduct = mapOf(
+                2..4 to 24,
+                1..4 to 24,
+                3..4 to 12,
+                100..100 to 100
+        )
         for ((range, product) in rangeToProduct)
             Assertions.assertEquals(product, range.product())
     }
+
+    @Test
+    fun `Test products of different Int Collections`() {
+        val collectionToProduct = mapOf(
+                listOf(1,2,3) to 6,
+                listOf(2,3) to 6,
+                listOf(3) to 3,
+                listOf(10, 10, 10) to 1000
+        )
+        for ((collection, product) in collectionToProduct)
+            Assertions.assertEquals(product, collection.product())
+    }
+
+
 }
