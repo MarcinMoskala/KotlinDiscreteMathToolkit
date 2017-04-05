9c1928ef29c33630deaf5008df25176bfe8ffa77
Marcin Moskala <marcinmoskala@gmail.com>
2017-04-05 12:29:50 +0200

    Refactor

--- src/com/marcinmoskala/math/tests/CombinationTest.kt
+++ src/com/marcinmoskala/math/tests/CombinationTest.kt
@@ -1,18 +1,23 @@
 package com.marcinmoskala.math.tests
 
 import com.marcinmoskala.math.combinationNumber
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 internal class CombinationTest {
 
     @Test fun `Test combination numbers for Sets with different sizes`() {
         val set = setOf(1, 2, 3, 4)
-        assertEquals(0, set.combinationNumber(0))
-        assertEquals(4, set.combinationNumber(1))
-        assertEquals(6, set.combinationNumber(2))
-        assertEquals(4, set.combinationNumber(3))
-        assertEquals(1, set.combinationNumber(4))
-        assertEquals(1, set.combinationNumber(5))
+        val subsetSizeToCombinationNumber = mapOf(
+                0 to 0L,
+                1 to 4L,
+                2 to 6L,
+                3 to 4L,
+                4 to 1L,
+                5 to 1L
+        )
+        for ((subsetSize, expectedCombinationNumber) in subsetSizeToCombinationNumber) {
+            Assertions.assertEquals(expectedCombinationNumber, set.combinationNumber(subsetSize))
+        }
     }
 }
