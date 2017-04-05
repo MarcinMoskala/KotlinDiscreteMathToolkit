6ae1ff89750a2d6595d508523716c38fa79833a8
Marcin Moskala <marcinmoskala@gmail.com>
2017-04-05 13:40:10 +0200

    Refactor

--- src/com/marcinmoskala/math/DiscreteMath.kt
+++ src/com/marcinmoskala/math/DiscreteMath.kt
@@ -22,12 +22,15 @@ fun <T> List<T>.permutationNumber(): Long = if (size < 1) 0L else factorial(size
 
 fun <T> Set<T>.allPermutations(): Set<List<T>> = toList().allPermutations()
 
-fun <T> List<T>.allPermutations(): Set<List<T>> {
-    if (isEmpty()) return setOf()
-    val element = get(0)
-    if (size == 1) return setOf(listOf(element))
-    val other = drop(1).allPermutations()
-    return other.flatMap { sublist -> (0..sublist.size).map { i -> sublist.plusAt(i, element) } }.toSet()
+fun <T> List<T>.allPermutations(): Set<List<T>> = when {
+    isEmpty() -> setOf()
+    size == 1 -> setOf(listOf(get(0)))
+    else -> {
+        val element = get(0)
+        drop(1).allPermutations()
+                .flatMap { sublist -> (0..sublist.size).map { i -> sublist.plusAt(i, element) } }
+                .toSet()
+    }
 }
 
 fun <T> List<T>.plusAt(index: Int, element: T): List<T> = when {
