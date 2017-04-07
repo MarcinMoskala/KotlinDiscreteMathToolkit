7e123dd173e54afbbf0f07d6e5d2d18c742efb59
Marcin Moskala <marcinmoskala@gmail.com>
2017-04-07 10:42:57 +0200

    Refactor

--- src/com/marcinmoskala/math/SublistsFun.kt
+++ src/com/marcinmoskala/math/SublistsFun.kt
@@ -1,7 +1,6 @@
 package com.marcinmoskala.math
 
 fun <T> Set<T>.subsets(): Set<Set<T>> = when {
-    size == 0 -> setOf(setOf())
-    size == 1 -> setOf(this, setOf())
+    size <= 1 -> setOf(setOf<T>()).plusElement(this)
     else -> drop(1).toSet().subsets().let { it + it.map { it + first() } }
 }
