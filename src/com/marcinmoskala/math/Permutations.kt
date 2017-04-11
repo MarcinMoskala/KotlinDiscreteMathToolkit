ec46f93814a845f0b36a09f24cde92ea7d4b1ef4
Marcin Moskala <marcinmoskala@gmail.com>
2017-04-11 21:25:01 +0200

    Make plusAt to be private in Permutations where it is used. This toolkit shoildn't provide outside this kind of methods because it is not connected to Discrete Math. If it would be used in other functions in the future then it should me moved to Utills and it's visibility should be changed to internal.

--- src/com/marcinmoskala/math/Permutations.kt
+++ src/com/marcinmoskala/math/Permutations.kt
@@ -25,3 +25,10 @@ fun <T> List<T>.allPermutations(): Set<List<T>> = when {
                 .toSet()
     }
 }
+
+private fun <T> List<T>.plusAt(index: Int, element: T): List<T> = when {
+    index > size || index < 0 -> throw Error("Cannot put at index $index because size is $size")
+    index == 0 -> listOf(element) + this
+    index == size -> this + element
+    else -> drop(index) + element + dropLast(size - index)
+}
