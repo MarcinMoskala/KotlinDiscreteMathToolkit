a7e63b2d38cb8e81f80cafaa921d234990d94306
Marcin Moskala <marcinmoskala@gmail.com>
2017-04-11 21:14:49 +0200

    Extract Permutations to separate file

--- src/com/marcinmoskala/math/DiscreteMathFun.kt
+++ src/com/marcinmoskala/math/DiscreteMathFun.kt
@@ -1,22 +1,5 @@
 package com.marcinmoskala.math
 
-fun <T> Set<T>.permutationNumber(): Long = factorial(size)
-
-fun <T> List<T>.permutationNumber(): Long = if (size < 1) 0L else factorial(size) / groupBy { it }.map { factorial(it.value.size) }.product()
-
-fun <T> Set<T>.allPermutations(): Set<List<T>> = toList().allPermutations()
-
-fun <T> List<T>.allPermutations(): Set<List<T>> = when {
-    isEmpty() -> setOf()
-    size == 1 -> setOf(listOf(get(0)))
-    else -> {
-        val element = get(0)
-        drop(1).allPermutations()
-                .flatMap { sublist -> (0..sublist.size).map { i -> sublist.plusAt(i, element) } }
-                .toSet()
-    }
-}
-
 fun <T> List<T>.plusAt(index: Int, element: T): List<T> = when {
     index > size || index < 0 -> throw Error("Cannot put at index $index because size is $size")
     index == 0 -> listOf(element) + this
