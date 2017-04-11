90403ca1ed8e377cd4f33a967b8d2d106d6e3dde
Marcin Moskala <marcinmoskala@gmail.com>
2017-04-11 21:20:37 +0200

    Add comments to Permutations

--- src/com/marcinmoskala/math/Permutations.kt
+++ src/com/marcinmoskala/math/Permutations.kt
@@ -1,11 +1,20 @@
 package com.marcinmoskala.math
 
+/**
+ *  Permutations are all different ways to arrange elements from some collection (https://en.wikipedia.org/wiki/Permutation).
+ *  For sets it's number is n!, for lists it is n! / (n1! * n2! * ...) where n1, n2... are numbers elements that are the same.
+ */
+
+/* This function returns number of all permutations of elements from set. It is equal to n! where n is size of set. */
 fun <T> Set<T>.permutationNumber(): Long = factorial(size)
 
+/* This function returns number of all permutations of elements from list. It is equal to n! / (n1! * n2! * ...) where n1, n2... are numbers elements that are the same. */
 fun <T> List<T>.permutationNumber(): Long = if (size < 1) 0L else factorial(size) / groupBy { it }.map { factorial(it.value.size) }.product()
 
+/* This function returns all permutations of elements from set. These are different ways to arrange elements from this list.  */
 fun <T> Set<T>.allPermutations(): Set<List<T>> = toList().allPermutations()
 
+/* This function returns all permutations of elements from list. These are different ways to arrange elements from this list.  */
 fun <T> List<T>.allPermutations(): Set<List<T>> = when {
     isEmpty() -> setOf()
     size == 1 -> setOf(listOf(get(0)))
