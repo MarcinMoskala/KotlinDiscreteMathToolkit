8e159a8bf988c8d3c97d463e43ae81ad4665fbb2
Marcin Moskala <marcinmoskala@gmail.com>
2017-04-20 15:49:29 +0200

    Refactor

--- src/com/marcinmoskala/math/tests/com/marcinmoskala/math/tests/PowersetTest.kt
+++ src/com/marcinmoskala/math/tests/com/marcinmoskala/math/tests/PowersetTest.kt
@@ -4,7 +4,7 @@ import com.marcinmoskala.math.powerset
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-internal class SubsetsTest {
+internal class PowersetTest {
 
     @Test fun `Sublists from empty list is only empty list`() {
         val emptyList = setOf<Int>()
@@ -12,21 +12,7 @@ internal class SubsetsTest {
     }
 
     @Test
-    fun `Sublist simple list test`() {
-        val set = setOf(
-                setOf(1, 2, 3),
-                setOf(1, 2),
-                setOf(1, 3),
-                setOf(2, 3),
-                setOf(1),
-                setOf(2),
-                setOf(3),
-                setOf())
-        assertEquals(set, setOf(1, 2, 3).powerset().toSet())
-    }
-
-    @Test
-    fun `Simple list combine test`() {
+    fun `Powerset simple example test`() {
         val set = setOf(
                 setOf(1, 2, 3),
                 setOf(1, 2),
@@ -37,5 +23,17 @@ internal class SubsetsTest {
                 setOf(3),
                 setOf())
         assertEquals(set, setOf(1, 2, 3).powerset())
+    }
+
+    @Test
+    fun `Size of n element set powerset is 2^n`() {
+        for(n in 1..6) {
+            val set = (1..n).toSet()
+            assertEquals(pow(2, n), set.powerset().size)
+        }
+    }
+
+    fun pow(num: Int, power: Int): Int {
+        return Math.pow(num.toDouble(), power.toDouble()).toInt()
     }
 }
