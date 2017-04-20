1b88cdecb3836cb6dffb6d6b2270910e4401921f
Marcin Moskala <marcinmoskala@gmail.com>
2017-04-20 15:08:25 +0200

    Simple set splits examples are calculated correctly.

--- src/com/marcinmoskala/math/tests/com/marcinmoskala/math/tests/SplitFuncTest.kt
+++ src/com/marcinmoskala/math/tests/com/marcinmoskala/math/tests/SplitFuncTest.kt
@@ -8,7 +8,7 @@ internal class SplitFuncTest {
 
     @Test
     fun `n elements can be only one way splitted to one or n groups`() {
-        for(n in 1..100) {
+        for (n in 1..100) {
             assertEquals(1, S(n, 1))
             assertEquals(1, S(n, n))
         }
@@ -17,8 +17,27 @@ internal class SplitFuncTest {
     @Test
     fun `For 0 splits, Stirling function is returning 1 for 0 and 0 otherwise`() {
         assertEquals(1, S(0, 0))
-        for(n in 1..100) {
+        for (n in 1..100) {
             assertEquals(0, S(n, 0))
         }
+    }
+
+    @Test
+    fun `Stirling function is correct according to recrrence defenition`() {
+        for (n in 1..10) {
+            for (k in 1..(n - 1)) {
+                assertEquals(S(n, k), S(n - 1, k - 1) + k * S(n - 1, k))
+            }
+        }
+    }
+
+    @Test
+    fun `Simple set splits examples are calculated correctly`() {
+        assertEquals(1, S(3, 1))
+        assertEquals(3, S(3, 2))
+        assertEquals(7, S(4, 2))
+        assertEquals(25, S(5, 3))
+        assertEquals(140, S(7, 5))
+        assertEquals(350, S(7, 4))
     }
 }
