8200a775c220a083c0fa0786537d42cab9e8d839
Marcin Moskala <marcinmoskala@gmail.com>
2017-05-03 14:21:14 +0200

    Add Java support for lists multiplication

--- src/main/java/com/marcinmoskala/math/IterableMultiplication.kt
+++ src/main/java/com/marcinmoskala/math/IterableMultiplication.kt
@@ -1,3 +1,5 @@
+@file:JvmName("DiscreteMath")
+@file:JvmMultifileClass
 package com.marcinmoskala.math
 
 operator fun <T, R> List<T>.times(l: List<R>): List<Pair<T, R>> = flatMap { e1 -> l.map { e2 -> e1 to e2 } }
