6adc977ad85460a4be5ac0b5ad963c4e7dfd57e6
Marcin Moskala <marcinmoskala@gmail.com>
2017-05-08 13:04:56 +0200

    Delete useless power function

--- src/main/java/com/marcinmoskala/math/Power.kt
+++ src/main/java/com/marcinmoskala/math/Power.kt
@@ -5,7 +5,3 @@ package com.marcinmoskala.math
 fun Int.pow(power: Int): Int {
     return Math.pow(this.toDouble(), power.toDouble()).toInt()
 }
-
-fun Int.pow(power: Double): Int {
-    return Math.pow(this.toDouble(), power).toInt()
-}
