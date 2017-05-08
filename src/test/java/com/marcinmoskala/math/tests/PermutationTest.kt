9c8f5cdd728201be6e2a25b6115a69821b49266a
Marcin Moskala <marcinmoskala@gmail.com>
2017-05-08 13:38:53 +0200

    Improve plusAt tests

--- src/test/java/com/marcinmoskala/math/tests/PermutationTest.kt
+++ src/test/java/com/marcinmoskala/math/tests/PermutationTest.kt
@@ -66,6 +66,8 @@ internal class PermutationTest {
     @Test fun `Simple plusAt tests`() {
         val list = listOf(1,2,3)
         assertIsThrowingError { list.plusAt(-1, 100) }
+        assertIsThrowingError { list.plusAt(-100, 100) }
+        assertIsThrowingError { list.plusAt(1000, 100) }
         assertEquals(listOf(100, 1, 2, 3), list.plusAt(0, 100))
         assertEquals(listOf(1, 100, 2, 3), list.plusAt(1, 100))
         assertEquals(listOf(1, 2, 3, 100), list.plusAt(3, 100))
