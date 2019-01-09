31fc0e08afe2349f90fd7083efb825c40e937fb6
Lars Artmann <LartyHD@gmx.de>
2019-01-09 17:11:00 +0100

    update Power part #3

--- src/main/java/com/marcinmoskala/math/Power.kt
+++ src/main/java/com/marcinmoskala/math/Power.kt
@@ -2,4 +2,21 @@
 @file:JvmMultifileClass
 package com.marcinmoskala.math
 
-fun Int.pow(power: Int): Int = Math.pow(this.toDouble(), power.toDouble()).toInt()
+import java.math.BigDecimal
+import java.math.BigInteger
+
+infix fun Number.pow(power: Number): Double = Math.pow(this.toDouble(), power.toDouble())
+
+infix fun BigInteger.pow(power: BigInteger): BigInteger = pow(power).toBigInteger()
+
+infix fun BigDecimal.pow(power: BigDecimal): BigDecimal = pow(power).toBigDecimal()
+
+infix fun Double.pow(power: Double): Double = pow(power)
+
+infix fun Long.pow(power: Long): Long = pow(power).toLong()
+
+infix fun Int.pow(power: Int): Int = pow(power).toInt()
+
+infix fun Short.pow(power: Short): Short = pow(power).toShort()
+
+infix fun Byte.pow(power: Byte): Byte = pow(power).toByte()
