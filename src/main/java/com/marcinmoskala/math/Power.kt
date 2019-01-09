c26adcc4b1f959413c6ef97855597f44a6da52d6
Lars Artmann <LartyHD@gmx.de>
2019-01-09 16:57:09 +0100

    Added infix to Int.pow(power: Int): Int

--- src/main/java/com/marcinmoskala/math/Power.kt
+++ src/main/java/com/marcinmoskala/math/Power.kt
@@ -2,4 +2,4 @@
 @file:JvmMultifileClass
 package com.marcinmoskala.math
 
-fun Int.pow(power: Int): Int = Math.pow(this.toDouble(), power.toDouble()).toInt()
+infix fun Int.pow(power: Int): Int = Math.pow(this.toDouble(), power.toDouble()).toInt()
